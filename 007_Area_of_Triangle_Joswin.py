56e7c806c8b46bf0cb430c769ce143b20d82d15b
joswinemmanuel <joswinemmanuel2002@gmail.com>
2025-05-14 17:46:49 +0530

    Area of a triangle

---
+++ 007_Area_of_Triangle_Joswin.py
@@ -0,0 +1,7 @@
+def area_of_triangle(b, h):
+    return (1/2) * b * h
+
+b = float(input("Enter base of triangle : "))
+h = float(input("Enter height of triangle : "))
+
+print(area_of_triangle(b, h))

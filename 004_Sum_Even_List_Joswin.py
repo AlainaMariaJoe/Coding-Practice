bd7137b42aa47964d770979ef209f7216b4b0f9b
joswinemmanuel <joswinemmanuel2002@gmail.com>
2025-05-14 14:48:32 +0530

    List Even Number sum

---
+++ 004_Sum_Even_List_Joswin.py
@@ -0,0 +1,14 @@
+def list_even_sum(l):
+    return sum([i for i in l if i % 2 == 0])
+
+# l = []
+# n = int(input("How many elements in list : "))
+# for i in range(n):
+#     l.append(int(input(f"Enter element {i+1} : ")))
+
+l = list(map(int, input("Enter List : ").split(",")))
+# Input - Enter List : 1, 2, 3, 4
+
+print(l)
+print(list_even_sum(l))
+# Output - 6

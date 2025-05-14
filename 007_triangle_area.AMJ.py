d5fb9219a5a5aa0542937c200b112b2d1331e401
Alaina Maria Joe <alainamariajoe33@gmail.com>
2025-05-14 17:49:46 +0530

    Area of triangle

---
+++ 007_triangle_area.AMJ.py
@@ -0,0 +1,6 @@
+def tr_area(b, h):
+    return 0.5 * b * h
+
+breadth = float(input("Enter the breadth of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+print(tr_area(breadth, height))

27f36b126c1a64cba70f48aca776fc201cfae371
joswinemmanuel <joswinemmanuel2002@gmail.com>
2025-05-15 11:06:56 +0530

    Largest of three

---
+++ 011_Largest_of_three_joswin.py
@@ -0,0 +1,28 @@
+def largest_of_three_1(a, b, c):
+    return max(a, b, c)
+
+def largest_of_three_2(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > c:
+        return b
+    else:
+        return c
+    
+def largest_of_three_3(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+        else:
+            return c
+    else:
+        if b > c:
+            return b
+        else:
+            return c
+    
+a, b, c = map(int, input("Enter three numbers : ").split(","))
+
+print(largest_of_three_1(a, b, c))
+print(largest_of_three_2(a, b, c))
+print(largest_of_three_3(a, b, c))

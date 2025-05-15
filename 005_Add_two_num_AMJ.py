18c6fe08d9673aff2e661e40db7eb0cbabd240bd
Alaina Maria Joe <alainamariajoe33@gmail.com>
2025-05-15 07:58:34 +0530

    Add two number

---
+++ 005_Add_two_num_AMJ.py
@@ -0,0 +1,9 @@
+def add_two_num(a, b):
+    return a + b
+def add_num(a, b):
+    return sum([a, b])
+
+n1 = float (input("Enter first number: "))
+n2 = float (input("Enter second number: "))
+print (add_two_num (n1, n2))
+print (add_num (n1, n2))

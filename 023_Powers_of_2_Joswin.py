30be52149762b16a369848397a25cd8eb106d4c8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2025-05-18 19:50:37 +0530

    Powers of two using Lambda

--- 023_Powers_of_2_Joswin.py
+++ 023_Powers_of_2_Joswin.py
@@ -1,5 +1,5 @@
 def powers_of_two(n):
     return list(map(lambda x: 2 ** x, range(n)))
 
-n = int(input("Enter number of terms : "))
-print(powers_of_two(n))
+terms = int(input("Enter number of terms : "))
+print(powers_of_two(terms))

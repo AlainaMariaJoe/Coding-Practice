292d8bcd6b81bcd9099fae81e82e0427af6860d3
Alaina Maria Joe <alainamariajoe33@gmail.com>
2025-05-23 13:30:59 +0530

    Sorts text by letter case and spaces

--- 045_Sort_character_type_AMJ.py
+++ 045_Sort_character_type_AMJ.py
@@ -1,4 +1,4 @@
-def sort_char(s):
+def sort_char_str(s):
     Upper_str = ''
     lower_str = ''
     space_str = ''
@@ -14,6 +14,23 @@ def sort_char(s):
             others_str += i
     return Upper_str + lower_str + space_str + others_str
 
+def sort_char_list(s):
+    Upper_list = []
+    lower_list = []
+    space_list = []
+    others_list = []
+    for i in s:
+        if i.isupper():
+            Upper_list.append(i)
+        elif i.islower():
+            lower_list.append(i)
+        elif i.isspace():
+            space_list.append(i)
+        else:
+            others_list.append(i)
+    return ''.join(Upper_list + lower_list + space_list + others_list)
+
 inp_str = input("Enter a string: ")
-print(f"'{sort_char(inp_str)}'")
+print(f"'{sort_char_str(inp_str)}'")
+print(f"'{sort_char_list(inp_str)}'")
 

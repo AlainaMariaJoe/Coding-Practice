3c834cef8544d5c2b845b39635a1e0bc13f45290
joswinemmanuel <joswinemmanuel2002@gmail.com>
2025-05-24 21:22:56 +0530

    Rearrange string and sort

---
+++ 045_Sort_String_Joswin.py
@@ -0,0 +1,18 @@
+def sort_and_rearrange_string(s):
+    upper = ''
+    lower = ''
+    space = ''
+    other = ''
+    for i in sorted(s):
+        if i.isupper():
+            upper += i
+        elif i.islower():
+            lower += i
+        elif i == ' ':
+            space += i
+        else:
+            other += i
+    return upper + lower + other + space
+
+s = input("Enter the string : ")
+print(sort_and_rearrange_string(s))

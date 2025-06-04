4f8ce7b82b160d99103fee2779894e363ec8640a
Alaina Maria Joe <alainamariajoe33@gmail.com>
2025-06-04 22:50:19 +0530

    Reverse a Word

--- 040_Word_reverse_AMJ.py
+++ 040_Word_reverse_AMJ.py
@@ -1,6 +1,6 @@
-def Reverse_word(s):
+def Rev_word(s):
     return ' '.join(s.split()[::-1])
 
 inp = input("Enter a string to reverse: ")
 print(f"Before reverse: {inp}")
-print(f"After reverse: {Reverse_word(inp)}")
+print(f"After reverse: {Rev_word(inp)}")

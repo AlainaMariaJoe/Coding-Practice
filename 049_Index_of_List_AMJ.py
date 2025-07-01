3098edf9d0a270076d043b1e9aff91a404eb6f0c
Alaina Maria Joe <alainamariajoe33@gmail.com>
2025-07-01 08:46:12 +0530

    Accessing Index of List

---
+++ 049_Index_of_List_AMJ.py
@@ -0,0 +1,7 @@
+l = [1, 7, 10, 31, 2]
+for ind, val in enumerate(l):
+    print(f"Index: {ind}, value:{val}")
+
+l = ["Alaina", "Maria", "Joe"]
+for i in range(len(l)):
+    print(f"Index: {i+1}, Value: {l[i]}")
